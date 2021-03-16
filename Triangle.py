8176199398e15bac4d6debd4046b89231fc77d3d
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2021-03-16 18:32:51 -0400

    Final commit

--- Triangle.py
+++ Triangle.py
@@ -4,14 +4,16 @@
 """
 def classify_triangle(side_a: int, side_b: int, side_c: int) -> str:
     """ Classifying the type of triangle depending on the values given to a,b,c """
-    if not(isinstance(side_a,int) or isinstance(side_b,int) or isinstance(side_c,int)) or int(side_a) <= 0 or int(side_b) <= 0 or int(side_c) <= 0:
+    if (not(isinstance(side_a,int) or isinstance(side_b,int) or isinstance(side_c,int))
+        or int(side_a) <= 0 or int(side_b) <= 0 or int(side_c) <= 0):
         raise ValueError("All values should be integers greater than zero")
-    elif side_a+side_b >= side_c and side_b+side_c >= side_a and side_c+side_a >= side_b:
+    if side_a+side_b >= side_c and side_b+side_c >= side_a and side_c+side_a >= side_b:
         if side_a == side_b == side_c:
             return "Equilateral triangle"
         elif side_a == side_b or side_b == side_c or side_c == side_a:
             return "Isosceles triangle"
-        elif side_a**2 == side_b**2 + side_c**2 or side_b**2 == side_c**2 + side_a**2 or side_c**2 == side_a**2 + side_b**2:
+        elif (side_a**2 == side_b**2 + side_c**2
+             or side_b**2 == side_c**2 + side_a**2 or side_c**2 == side_a**2 + side_b**2):
             return "Right angle triangle"
         else:
             return "Scalen triangle"
